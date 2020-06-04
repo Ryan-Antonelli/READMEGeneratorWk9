1e7f0b2489ac11a1517a43325c726bfbe2ba065a
Ryan Antonelli <ryan.a.antonelli@gmail.com>
2020-06-03 17:26:23 -0700

    questions added to index.js

---
+++ index.js
@@ -0,0 +1,66 @@
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?"
+  },
+  {
+    type: "input",
+    name: "URL",
+    message: "the URL of your project?"
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of your project?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project"
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How is your project installed?"
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be run to run tests?",
+    default: "npm test"
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "How can a user contribute to the project?",
+  }
+];
+
+
+
+function writeToFile(fileName, data) {
+    
+}
+
+function init() {
+
+}
+
+init();
